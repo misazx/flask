63a810ceca350f0704999797420d486dec5f47b1
Administrator <415161584@qq.com>
2019-09-23 20:34:01 +0800

    dd

--- webSocket.py
+++ webSocket.py
@@ -15,11 +15,27 @@ app.config['SECRET_KEY'] = secret_key  # 配置会话密钥
 app.config['SESSION_TYPE'] = "redis"  # session类型为redis
 app.config['SESSION_PERMANENT'] = True  # 如果设置为True，则关闭浏览器session就失效
 # 访问远程redis
-# app.config['SESSION_REDIS'] = redis.Redis(host='ec2-3-215-116-159.compute-1.amazonaws.com', port='11249', password='p790653b7cce68ee0a7f855d8cc316be7976835ae1b1099c462bcdb0e7c2c12ed')
-app.config['SESSION_REDIS'] = redis.from_url(os.environ['REDIS_URL'])
+# app.config['SESSION_REDIS'] = redis.from_url(os.environ['REDIS_URL'])
+r= redis.Redis(host='127.0.0.1', port='6379',db=1, decode_responses=True)
+app.config['SESSION_REDIS'] = r
 app.config.from_object(__name__)
 Session(app)
-socket_io = SocketIO(app, logger=True, engineio_logger=True)
+socket_io = SocketIO(app)#, logger=True, engineio_logger=True)
+
+
+r_whoisghost_lobby = 'whoisghost_lobby'
+
+@app.route("/whoisghost/lobby/list ", methods=['post', 'get'])
+def game_whoisghost_lobby_list():
+    mes= r.lrange(r_whoisghost_lobby,0,-1)
+    return mes
+
+@app.route("/whoisghost/lobby/create ", methods=['post', 'get'])
+def game_whoisghost_lobby_create():
+    data = request.args['data']
+    mes=data
+    r.lpushx(r_whoisghost_lobby,mes)
+    return mes
 
 
 @app.route("/listen", methods=['post', 'get'])
